d4f1d1b2e702c59f5d4b7286afa5f635f49a47af
Nadia Hachemi <nadia_hachemi@live.fr>
2018-07-09 15:50:45 +0200

    done

--- starter-code/iteration-1/main.ts
+++ starter-code/iteration-1/main.ts
@@ -1,18 +1,33 @@
 // Iteration 1
 // 1. Create an array of strings
+var tasks:Array<string> =[];
+
 
 // 2. Create a function to add a task in to the array, call it `addTask`, the function has to accept a string and don't have to return anything.
-
+function addTask (task: string) {
+    tasks.push(task);
+    console.log(task+ " has been added")
+    return tasks.length
+} 
 // 3. Create a function to list all tasks, it must show in the console de task.
-
+function listAllTasks(list: Array<string>){
+    for (var i = 0; i < list.length; i++) {
+        console.log(list[i])
+    }
+}
 // 4. Create a function to delete a task, you must find the task inside the array and delete it.
 
+function deleteTask (task: string){
+    tasks.splice(tasks.indexOf(task));
+    return tasks.length;
+
+}
 // Execution
 console.log("Number of items:", addTask('This is our first task'));
 console.log("Number of items:", addTask('Eat pizza 🍕 yummy!!!'));
 console.log("Number of items:", addTask('Finish this iteration 1!! 🤓'));
 console.log("Number of items:", addTask('Finish this iteration 2!! 🤓'));
 console.log("Number of items:", addTask('Finish this iteration 3!! 🤓'));
-listAllTasks();
+listAllTasks(tasks)
 console.log("Number of items:", deleteTask('Finish this iteration 1!! 🤓'));
-listAllTasks();
+listAllTasks(tasks)
--- starter-code/iteration-2/main.ts
+++ starter-code/iteration-2/main.ts
@@ -1,6 +1,33 @@
 // Add the reference to the "TodoInterface"
-
+import {TodoInterface} from "./todoInterface"
+// interface TodoInterface{
+//     tasks:Array<string>;
+//     addTask (task: string);
+//     listAllTask();
+//     deleteTask( task:string);
+// }
 // 1. Create a class Todo that implements the Interface created before.
+class Todo implements TodoInterface{
+    constructor(
+        public tasks=[]
+    )
+    {}
+
+    listAllTask(){
+        console.log(this.tasks.length)
+    };
+
+    addTask (task: string){
+        this.tasks.push(task);
+        console.log(task + " has been added")
+        return this.tasks.length
+    }
+
+    deleteTask( task:string){
+        this.tasks.splice(this.tasks.indexOf(task));
+        return this.tasks.length;
+    }
+};
 
 // Execution
 let myTodos = new Todo();
@@ -9,7 +36,7 @@ console.log("Number of items:", myTodos.addTask('Eat pizza 🍕 yummy!!!'));
 console.log("Number of items:", myTodos.addTask('Finish this iteration 1!! 🤓'));
 console.log("Number of items:", myTodos.addTask('Finish this iteration 2!! 🤓'));
 console.log("Number of items:", myTodos.addTask('Finish this iteration 3!! 🤓'));
-myTodos.listAllTasks();
+myTodos.listAllTask();
 console.log("Number of items:", myTodos.deleteTask('Finish this iteration 1!! 🤓'));
 console.log("Number of items:", myTodos.deleteTask('Finish this iteration 2!! 🤓'));
-myTodos.listAllTasks();
+myTodos.listAllTask();
